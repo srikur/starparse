779459a5cc3b39427e78ab7d1e0fbe1b8cd8ee06
srikur <srikur@users.noreply.github.com>
2026-09-12 19:55:27 -0400

    Add basic test case

--- tests/test_container.cpp
+++ tests/test_container.cpp
@@ -4,7 +4,7 @@
 
 struct Args {
     [[=StarParse::Opt{'v', "Vector test"}]] std::vector<std::string> vec;
-    [[=StarParse::Opt{'a', "Array test"}, =StarParse::Separator{";"}]] std::array<std::string, 4> arr;
+    [[=StarParse::Opt{'a', "Array test"}, =StarParse::Separator{","}]] std::array<std::string, 4> arr;
 };
 
 auto main(int argc, char **argv) -> int {
