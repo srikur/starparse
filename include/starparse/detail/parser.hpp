e9754d5aebddc0830f6c2af493954f296e10cc3b
srikur <srikur@users.noreply.github.com>
2026-09-17 05:41:55 -0500

    Remove static from version

--- include/starparse/detail/parser.hpp
+++ include/starparse/detail/parser.hpp
@@ -161,7 +161,7 @@ namespace StarParse::detail::Parser {
             return show_version_;
         }
 
-        [[nodiscard]] static std::string version() {
+        [[nodiscard]] std::string version() {
             constexpr auto program = program_of(^^T);
             if constexpr (program.has_value()) {
                 return std::format("{} version {}", program->name, program->version);
